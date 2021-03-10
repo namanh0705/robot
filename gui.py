6b9d772f502ab7ce370ef3a0723cfefc36266c01
namanh0705 <namproanh0705@gmail.com>
2021-03-10 16:55:19 +0700

    ad resolution

--- gui.py
+++ gui.py
@@ -24,8 +24,8 @@ taudanglamhang = "http://tracuucontainer.danangport.com:40004/p/tau-dang-lam-han
 
 top = 0
 left = 0
-wight = 900
-height = 600
+wight = 1280
+height = 900
 
 
 class RobotFaceWindow(QMainWindow):
